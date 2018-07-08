ac3cf569c424a5a757902dbd8ab1398ee7aa2e78
lightingLu <215154863@qq.com>
2018-07-08 09:07:59 +0800

    create project

--- app/src/main/java/com/siqiyan/lightlu/eyepetizercode/MainActivity.kt
+++ app/src/main/java/com/siqiyan/lightlu/eyepetizercode/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        test()
+    }
+
+    private fun test() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
