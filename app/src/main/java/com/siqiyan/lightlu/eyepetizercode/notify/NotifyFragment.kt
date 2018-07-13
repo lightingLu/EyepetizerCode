466521aef2293db86d1a48616d9fe99ca67770d9
lightingLu <215154863@qq.com>
2018-07-14 00:30:00 +0800

    完成通知界面

--- app/src/main/java/com/siqiyan/lightlu/eyepetizercode/notify/NotifyFragment.kt
+++ app/src/main/java/com/siqiyan/lightlu/eyepetizercode/notify/NotifyFragment.kt
@@ -1,7 +1,11 @@
 package com.siqiyan.lightlu.eyepetizercode.notify
 
+import android.content.Intent
+import android.graphics.Typeface
 import com.siqiyan.lightlu.eyepetizercode.R
 import com.siqiyan.lightlu.eyepetizercode.base.BaseFragment
+import com.siqiyan.lightlu.eyepetizercode.search.SearchActivity
+import kotlinx.android.synthetic.main.notify_fragment.*
 
 /**
  * 创建日期：2018/7/8 on 22:13
@@ -10,10 +14,14 @@ import com.siqiyan.lightlu.eyepetizercode.base.BaseFragment
  * 类说明：
  */
 class NotifyFragment :BaseFragment() {
-    override fun initDate() {
-    }
+    override fun initDate() =iv_search.setOnClickListener({
+        v ->  var intent =Intent(activity,SearchActivity::class.java)
+        startActivity(intent)
+    })
 
     override fun initView() {
+        tv_bar_title.typeface = Typeface.createFromAsset(activity!!.assets, "fonts/Lobster-1.4.otf")
+
     }
 
 
