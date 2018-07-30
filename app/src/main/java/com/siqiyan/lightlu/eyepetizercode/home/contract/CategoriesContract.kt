1f044639c9e87307b5bef4eb39e1663aa39ef897
lightingLu <215154863@qq.com>
2018-07-30 23:27:51 +0800

    contract

---
+++ app/src/main/java/com/siqiyan/lightlu/eyepetizercode/home/contract/CategoriesContract.kt
@@ -0,0 +1,23 @@
+package com.siqiyan.lightlu.eyepetizercode.home.contract
+
+import com.siqiyan.lightlu.eyepetizercode.base.BasePrsenter
+import com.siqiyan.lightlu.eyepetizercode.base.BaseView
+import com.siqiyan.lightlu.eyepetizercode.net.entity.Categories
+import io.reactivex.disposables.Disposable
+
+/**
+ * 创建日期：2018/7/9 on 23:45
+ * @author ludaguang
+ * @version 1.0
+ * 类说明：
+ */
+interface CategoriesContract {
+    interface CategoriesPresenter : BasePrsenter {
+        fun categories(): Disposable
+    }
+
+    interface CategoriesView : BaseView<CategoriesPresenter> {
+        fun onCategoriesSucc(result: List<Categories>)
+        fun onCategoriesFail(error: Throwable?)
+    }
+}
